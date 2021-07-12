b8b923fa89ccc2e8936a88e8e3cf9fe5f590e79f
Lucas André <lucasandrefreitas1@gmail.com>
2021-07-12 20:17:56 -0300

    Código inicial do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,63 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, AlurakutStyles, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'; 
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar({ avatar }) {
+  return (
+    <Box>
+      <img src={`https://github.com/${avatar}.png`} alt="Profile Image" style={{ borderRadius: '8px' }} />
+      <AlurakutProfileSidebarMenuDefault />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const usuarioAleatorio = 'llofyy';
+  const pessoasFavoritas = [
+    'luizomf', 
+    'rafaballerini', 
+    'marcobrunodev', 
+    'maykbrito', 
+    'filipedeschamps', 
+    'diego3g'
+  ];
+
+  return (
+    <>
+    <AlurakutMenu githubUser={usuarioAleatorio} />
+    <MainGrid>
+      <div className="profileArea" style={{gridArea: "profileArea"}}>
+       <ProfileSidebar avatar={usuarioAleatorio} />
+      </div>
+      <div className="welcomeArea" style={{gridArea: "welcomeArea"}}>
+        <Box>
+          <h1 className="title">
+            Bem Vindo(a)
+            <OrkutNostalgicIconSet />
+          </h1>
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{gridArea: "profileRelationsArea"}}>
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">
+            Pessoas da Comunidade ({pessoasFavoritas.length})
+          </h2>
+          <ul>
+            {pessoasFavoritas.map(pessoa => {
+              return (
+                <li key={pessoa}>
+                  <a href={`/users/${pessoa}`}>
+                  <img src={`https://github.com/${pessoa}.png`} />
+                  <span>{pessoa}</span>
+                </a>
+                </li>
+              )
+            })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+      </div>
+    </MainGrid>
+    </>
+  )
 }
